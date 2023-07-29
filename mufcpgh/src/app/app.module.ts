198955046bac5f60ddade8b17716aee74efeea5d
Sanjeev <optims96@gmail.com>
2023-07-29 17:04:38 -0400

    add photo gallery component with pictures

--- mufcpgh/src/app/app.module.ts
+++ mufcpgh/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LinkTreeComponent } from './home-page/link-tree/link-tree.component';
 import { FooterComponent } from './footer/footer.component';
+import { PhotoGalleryComponent } from './home-page/photo-gallery/photo-gallery.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './footer/footer.component';
     AboutUsComponent,
     HomePageComponent,
     LinkTreeComponent,
-    FooterComponent
+    FooterComponent,
+    PhotoGalleryComponent
   ],
   imports: [
     BrowserModule,
