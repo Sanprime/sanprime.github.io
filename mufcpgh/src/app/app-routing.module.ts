edd5711c856b2eee7799fd900a398e77f8c8984c
Sanjeev Ramanan <sanjeevramanan@sanjeevs-air.mynetworksettings.com>
2024-08-22 00:02:03 -0400

    remove useless componenets lol

--- mufcpgh/src/app/app-routing.module.ts
+++ mufcpgh/src/app/app-routing.module.ts
@@ -1,12 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {AboutUsComponent} from "./about-us/about-us.component";
 import {HomePageComponent} from "./home-page/home-page.component";
-import {LatestScoreComponent} from "./latest-score/latest-score.component";
 
 const routes: Routes = [
-  { path: 'about_us', component: AboutUsComponent},
-  { path: 'latest_score', component: LatestScoreComponent},
   { path: '', component: HomePageComponent}
 ];
 
--- mufcpgh/src/app/app.module.ts
+++ mufcpgh/src/app/app.module.ts
@@ -3,23 +3,19 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserModule} from "@angular/platform-browser";
 import {NgbModule} from "@ng-bootstrap/ng-bootstrap";
-import { AboutUsComponent } from './about-us/about-us.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LinkTreeComponent } from './home-page/link-tree/link-tree.component';
 import { FooterComponent } from './footer/footer.component';
 import { PhotoGalleryComponent } from './home-page/photo-gallery/photo-gallery.component';
-import { LatestScoreComponent } from './latest-score/latest-score.component';
 import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutUsComponent,
     HomePageComponent,
     LinkTreeComponent,
     FooterComponent,
-    PhotoGalleryComponent,
-    LatestScoreComponent
+    PhotoGalleryComponent
   ],
   imports: [
     BrowserModule,
