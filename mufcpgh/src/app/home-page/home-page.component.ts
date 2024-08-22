035315d93b3c8badad6fe008705e4f0776101f4b
Sanjeev Ramanan <sanjeevramanan@sanjeevs-air.mynetworksettings.com>
2024-08-21 23:48:54 -0400

    add twitter and youtube embedded widget

--- mufcpgh/src/app/home-page/home-page.component.ts
+++ mufcpgh/src/app/home-page/home-page.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
-
+export class HomePageComponent implements AfterViewInit {
+  ngAfterViewInit() {
+    const script = document.createElement('script');
+    script.src = 'https://platform.twitter.com/widgets.js';
+    script.async = true;
+    script.charset = 'utf-8';
+    document.body.appendChild(script);
+  }
 }
